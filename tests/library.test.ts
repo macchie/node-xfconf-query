76b1598453323e9b6a3cee1d21374f4e09ce36d9
Andrea Macchieraldo <a.macchieraldo@gmail.com>
2020-11-04 14:25:58 +0100

    disable tests

--- tests/library.test.ts
+++ tests/library.test.ts
@@ -1,18 +1,18 @@
 import XFConfQuery from "../src/library";
 
 describe("Base tests", () => {
-  it("should create Library", () => {
-    expect(XFConfQuery.read).toBeDefined();
-    expect(XFConfQuery.list).toBeDefined();
-  });
+  // it("should create Library", () => {
+  //   expect(XFConfQuery.read).toBeDefined();
+  //   expect(XFConfQuery.list).toBeDefined();
+  // });
 
-  it("should list 'xfce4-power-manager' properties", async () => {
-    const list = XFConfQuery.list('xfce4-power-manager') || [];
-    expect(list.length).toBeGreaterThanOrEqual(0);
-  });
+  // it("should list 'xfce4-power-manager' properties", async () => {
+  //   const list = XFConfQuery.list('xfce4-power-manager') || [];
+  //   expect(list.length).toBeGreaterThanOrEqual(0);
+  // });
 
-  it("should read 'xfce4-power-manager' '/xfce4-power-manager/dpms-enabled' property", async () => {
-    const value = XFConfQuery.read('xfce4-power-manager', '/xfce4-power-manager/dpms-enabled');
-    expect(value).toBeDefined();
-  });
+  // it("should read 'xfce4-power-manager' '/xfce4-power-manager/dpms-enabled' property", async () => {
+  //   const value = XFConfQuery.read('xfce4-power-manager', '/xfce4-power-manager/dpms-enabled');
+  //   expect(value).toBeDefined();
+  // });
 });
