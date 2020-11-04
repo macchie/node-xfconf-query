4a0b6cced265ab99d6afb86859dabbcbdd0b65bf
Andrea Macchieraldo <a.macchieraldo@gmail.com>
2020-11-04 13:47:10 +0100

    remove dist from git

--- dist/library.d.ts
+++
@@ -1,7 +0,0 @@
-export default class XFConfQuery {
-    static list(channel: string): string[] | void;
-    static read(channel: string, property: string): string | number | boolean | undefined | void;
-    static write(): Promise<void>;
-    private static getListCommand;
-    private static getReadCommand;
-}
